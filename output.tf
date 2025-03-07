b22879287e216feea0dd52d79775b8aaf222d801
venu222m <145119205+venu222m@users.noreply.github.com>
2025-03-07 08:08:22 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,29 @@
+===output.tf
+#we can find the server instance id & public ip 
+resource "aws_instance" "app_server" {
+  ami           = "ami-04b4f1a9cf54c11d0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+# variables.tf
+
+variable "instance_name" {
+  description = "Value of the Name tag for the EC2 instance"
+  type        = string
+  default     = "devops"
+}
+
+=========output.tf==============
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
